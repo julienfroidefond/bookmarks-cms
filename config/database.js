5e3340f12e47d1ccaaa9e2e2019e3da4570d10d5
Julien Froidefond <julien.froidefond@cdiscount.com>
2021-03-23 15:58:25 +0100

    setting postgre as db

--- config/database.js
+++ config/database.js
@@ -1,14 +1,22 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        schema: env("DATABASE_SCHEMA", "public"), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
       },
       options: {
-        useNullAsDefault: true,
+        ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
