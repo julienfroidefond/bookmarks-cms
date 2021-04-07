1b603b35bdfdeee53810bb8e40a46230f58c2805
Julien Froidefond <julien.froidefond@cdiscount.com>
2021-04-07 08:45:08 +0200

    feat(foldertree): adding orphans bookmarks at root
    query by hand to mush orphans at the root of the tree
    
    fix #23

--- api/folders/controllers/folders.js
+++ api/folders/controllers/folders.js
@@ -13,7 +13,7 @@ module.exports = {
     const { tag_id_in, no_empty_folders } = ctx.query;
     const bookmarksFilteredIds = new Set();
     if (tag_id_in) {
-      const tags = tag_id_in.split(',');
+      const tags = tag_id_in.split(",");
       let tagsEntities = await strapi.services.tags.find({ id_in: tags });
       tagsEntities = tagsEntities.map((entity) =>
         sanitizeEntity(entity, { model: strapi.models.tags })
@@ -38,15 +38,30 @@ module.exports = {
     });
     foldersEntities.forEach((entity) => {
       const { parent } = entity;
-      if (parent && (no_empty_folders && entity.bookmarks.length > 0 || !no_empty_folders)) {
-          tree[parent.id].children.push(entity);
+      if (
+        parent &&
+        ((no_empty_folders && entity.bookmarks.length > 0) || !no_empty_folders)
+      ) {
+        tree[parent.id].children.push(entity);
       }
     });
+
+    //Orphans : SELECT * from bookmarks left join bookmarks_folders__folders_bookmarks as folder on folder.bookmark_id = bookmarks.id where folder_id IS NULL
+    const knex = strapi.connections.default;
+    let orphans = await knex("bookmarks")
+      .leftJoin(
+        "bookmarks_folders__folders_bookmarks",
+        "bookmarks_folders__folders_bookmarks.bookmark_id",
+        "bookmarks.id"
+      )
+      .whereNull("bookmarks_folders__folders_bookmarks.folder_id")
+      .select("*");
 
     let result = [];
     tree.forEach((entity) => {
       if (!entity.parent) result.push(entity);
     });
+    result.push(...orphans);
     return result;
   },
 };
