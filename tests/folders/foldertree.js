5987533afce9130e0229f37ff3e05499dad9c42e
Julien Froidefond <julien.froidefond@cdiscount.com>
2021-04-08 08:40:48 +0200

    test: init
    initialisation of env and tests files + in comment the future test of tree
    
    re #25

---
+++ tests/folders/foldertree.js
@@ -0,0 +1,18 @@
+//TODO / IN PROGRESS
+
+// const request = require("supertest");
+
+// const jwt =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE1OTgxODU5LCJleHAiOjE2MTg1NzM4NTl9.06YN9zo3e5sg9zmuuRj-UvmoabMHLClcK1jpb6iKlH4";
+
+// it("should return hello world", async (done) => {
+//   await request(strapi.server) // app server is an instance of Class: http.Server
+//     .get("/folders/tree?no_empty_folders=true")
+//     .set("Authorization", `Bearer ${jwt}`)
+//     .expect(200) // Expect response http code 200
+//     .then((data) => {
+//       console.log(data);
+//       expect(data.text).toBe("Hello World!"); // expect the response text
+//     });
+//   done();
+// });
